d0faf39be01864d3ee25e0ca9f49da458a28e702
Zannatul Ferdous <zannat.ferdous25@gmail.com>
2021-03-04 17:06:42 -0500

    done

--- script.js
+++ script.js
@@ -6,22 +6,22 @@ const information = {
 const message_gen = obj => {
   let message = []
   for (let item in obj){
-  let num = Math.floor(Math.random() * obj[item].length);
-  switch (item) {
-    case 'name':
-      message.push(`Hello ${obj[item][num]}, how are you?`);
-      break;
-    case 'verb':
-      message.push(`What are you doing? ${obj[item][num]}`);
-      break;
-    case 'adjective':
-      message.push(`You look ${obj[item][num]}!`)
-      break;
-    default:
-      message.push(`This is done.`)
-      break;
+    let num = Math.floor(Math.random() * obj[item].length);
+    switch (item) {
+      case 'name':
+        message.push(`Hello ${obj[item][num]}, how are you?`);
+        break;
+      case 'verb':
+        message.push(`What are you doing? ${obj[item][num]}`);
+        break;
+      case 'adjective':
+        message.push(`You look ${obj[item][num]}!`)
+        break;
+      default:
+        message.push(`This is done.`)
+        break;
+      }
     }
-  }
   return console.log(message.join('\n'));
 };
 message_gen(information);
